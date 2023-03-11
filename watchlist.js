d0bdae13e5059b9b2b0eee7726ba811a050b2346
Chigozie Johnpaul Maduka <chigoziemaduka401@gmail.com>
2023-03-11 17:32:32 +0100

    fixed

--- watchlist.js
+++ watchlist.js
@@ -1,10 +1,6 @@
-import { filmIcon, filmDisplay1, searchInput, movieArray, searchFilm, renderHtml, handleAdd2Watchlist, handleAdd2Watchlist } from "./utils.js"
+import { filmIcon, filmDisplay1, searchInput, movieArray, searchFilm, renderHtml, handleAdd2Watchlist } from "./utils.js"
 
 const watchlistRender = document.querySelector('.watchlist-render')
 let getWatchlistMovies = JSON.parse(localStorage.getItem("watchlistMovies"))
 
-watchlistRender.innerHTML = `<img src="${getWatchlistMovies.Poster}">`
-
-document.getElementById('getm').addEventListener('click', () => {
-    console.log('getWatchlistMovies')
-})
+watchlistRender.innerHTML = renderHtml(getWatchlistMovies)
